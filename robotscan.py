87936c64945d5f67bcc7063535bf0d6629293cc0
GH0st3rs <GH0st3rs@users.noreply.github.com>
2017-05-08 19:39:48 +0300

    Add scan folder with 301 redirect

--- robotscan.py
+++ robotscan.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-
 from argparse import ArgumentParser, REMAINDER
 import requests
 import re
 import os
+import json
+from urllib.parse import urlparse
+
+HOST = ''
+REPORT = ''
 
 if not (os.path.exists('/usr/bin/dirsearch') or os.path.exists('/bin/dirsearch')):
     msg = """dirsearch not found
@@ -17,11 +21,12 @@ Use:
 
 def ParseArgs():
     parser = ArgumentParser()
-    parser.add_argument('-u', '--url', help='URL target')
-    parser.add_argument('-w', '--wordlist', help='Dictionary')
-    parser.add_argument('-e', '--extensions', help='Extension list separated by comma (example: php,asp)')
-    parser.add_argument('-x', '--excludestatus', help='Exclude status code, separated by comma (example: 301,500)')
-    parser.add_argument('-a', '--args', nargs=REMAINDER, help='Other args for dirsearch. Use quotes for parameters (example: "--timeout=0 -t 20")')
+    parser.add_argument('-u', '--url', required=True, help='URL target')
+    parser.add_argument('-w', '--wordlist', required=True, help='Dictionary')
+    parser.add_argument('-e', '--extensions', required=True, help='Extension list separated by comma (example: php,asp)')
+    parser.add_argument('-x', '--excludestatus', type=str, help='Exclude status code, separated by comma (example: 301,500)')
+    parser.add_argument('-a', '--args', nargs=REMAINDER,
+                        help='Other args for dirsearch. Use quotes for parameters (example: "--timeout=0 -t 20")')
     return parser.parse_args()
 
 
@@ -33,24 +38,34 @@ def GetRobots(url):
         txt = req.text.replace('Disallow: /', '').replace('Allow: /', '')
         comments = re.findall('#[^\n]+', txt)
         ua = re.findall('User-agent:[^\n]+', txt)
-        for item in comments: txt = txt.replace(item, '')
-        for item in ua: txt = txt.replace(item, '')
+        for item in comments:
+            txt = txt.replace(item, '')
+        for item in ua:
+            txt = txt.replace(item, '')
         file = open('robots.lst', 'w')
         file.write(txt)
         file.close()
 
 
 def Search(url, ext, wordlist, code='', args=''):
-    code = str(code)
     if len(code) == 0:
-        os.system('sudo dirsearch -r -f -u %s -e %s -w %s %s' % (url, ext, wordlist, args))
+        os.system('sudo dirsearch -r -f -u %s -e %s -w %s --json-report=%s %s' % (url, ext, wordlist, REPORT, args))
     else:
-        os.system('sudo dirsearch -r -f -u %s -e %s -w %s -x %s %s' % (url, ext, wordlist, code, args))
+        os.system('sudo dirsearch -r -f -u %s -e %s -w %s --json-report=%s -x %s %s' % (url, ext, wordlist, REPORT, code, args))
+    report = json.load(open(REPORT))
+    fil = filter(lambda x: x['status'] == 301, report[list(report)[0]])
+    urls = list(map(lambda x: x['redirect'], fil))
+    for item in urls:
+        Search(item, ext, wordlist, code, args)
 
 
 def main():
+    global HOST
+    global REPORT
     args = ParseArgs()
     URL = args.url
+    HOST = urlparse(URL).netloc
+    REPORT = os.path.join('/tmp', 'dirsearch_%s' % HOST)
     EXT = args.extensions
     WORDLIST = args.wordlist
     CODE = args.excludestatus
